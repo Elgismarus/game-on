795a7e2e783b912f0672b06867f72971ce2579b3
Alexandre Tran <elgismarus@gmail.com>
2018-01-16 09:05:06 +0000

    [Build] Remove quiet and set-upstream in commit

--- .travis/badge.sh
+++ .travis/badge.sh
@@ -6,9 +6,9 @@ set -e
 # Script to update the badge in README.md
 update_badge(){
 
-	echo "----------------------- UPDATE BADGE -----------------------"
+	printf "---------------------------- UPDATE BADGE ----------------------------"
 	# Should only be here if different branch
-	echo "Attempting to update badges in README.md from ${PWD} ..."
+	printf "Attempting to update badges in README.md from ${PWD} ..."
 
 	# Get all the links
 	GAMEON_FILE_LINKS=$(grep https README.md | sed 's/https/\nhttps/g' | grep ^https | sed 's/\(^https[^ <]*\)\(.*\)/\1/g' | grep branch= | tr -d '()[]' | sort -u)
@@ -18,24 +18,24 @@ update_badge(){
 	do
 		# Change branch link
 		temp=$(sed "s/branch=\(.*\)/branch=$TRAVIS_BRANCH/g" <<< $link)
-		echo "Changing link from ${link} to ${temp}"
+		printf "Changing link from ${link} to ${temp}"
 		sed -i "s,${link},${temp},g" README.md
 	done
 
-	echo "Badges link updated!"
-	echo "------------------------------------------------------------\n"
+	printf "Badges link updated!"
+	printf "----------------------------------------------------------------------\n"
 }
 
 commit_and_push(){
-	echo "--------------------- COMMIT AND PUSH ----------------------"
-	echo "Commit and pushing ${FILES} from ${PWD} ..."
+	printf "-------------------------- COMMIT AND PUSH ---------------------------"
+	printf "Commit and pushing ${FILES} from ${PWD} ..."
 	MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 	git add ${FILES}
 	git commit -m "[ci skip] ${MESSAGE}"
 	git remote add origin "https://${GITHUB_TOKEN}@github.com:${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
-	git push --quiet --set-upstream origin $TRAVIS_BRANCH 
-	echo "Commit and push done!"
-	echo "------------------------------------------------------------\n"
+	git push origin $TRAVIS_BRANCH 
+	printf "Commit and push done!"
+	printf "----------------------------------------------------------------------\n"
 }
 
 
@@ -44,10 +44,10 @@ GAMEON_FILE_BRANCH=$(awk -F'[()]' '{print $2}' README.md | awk -F'[=&]' '{print 
 
 # Compare branches
 if [ "$GAMEON_FILE_BRANCH" == "$TRAVIS_BRANCH" ]; then
-	echo "Same branch - Skipping badge update!"
+	printf "Same branch - Skipping badge update!"
 	exit 0
 else
-	echo "Update require for README.md required."
+	printf "Update require for README.md required."
 	update_badge
 	commit_and_push
 	exit 0
